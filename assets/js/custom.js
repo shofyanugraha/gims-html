bd697c1451c30fb6c8040366edd149759cfccdee
Rizky <rizkyshofyan@gmail.com>
2015-11-27 12:47:05 +0700

    adding 3d slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,29 @@
 $(function(){
-	Cute.ModuleLoader.css3d_files  =  ['assets/js/cute.css3d.module.js'];
-	Cute.ModuleLoader.canvas_files =  ['assets/js/cute.canvas.module.js'];
-	Cute.ModuleLoader.dom2d_files  =  ['assets/js/cute.2d.module.js'];
-	var slider = new Cute.Slider();
-	slider.setup('cute-slider' ,  'slider-wrapper');
-	if(self !== top)document.getElementById('alert').style.display = 'block';
+    var curSkin = 'borderlessdark3d';
+
+    $(document).ready(function() {
+        if (typeof $.fn.layerSlider == "undefined") {
+            lsShowNotice('layerslider_22', 'jquery');
+        } else {
+            $("#layerslider_22").layerSlider({
+                startInViewport: false,
+                pauseOnHover: false,
+                skin: 'borderlessdark3d',
+                globalBGColor: 'transparent',
+                hoverBottomNav: true,
+                lazyLoad: false,
+                yourLogoStyle: 'left: 10px; top: 10px;',
+                cbInit: function(element) {},
+                cbStart: function(data) {},
+                cbStop: function(data) {},
+                cbPause: function(data) {},
+                cbAnimStart: function(data) {},
+                cbAnimStop: function(data) {},
+                cbPrev: function(data) {},
+                cbNext: function(data) {},
+                skinsPath: 'assets/theme/',
+                thumbnailNavigation: 'hover'
+            })
+        }
+    });
 });
